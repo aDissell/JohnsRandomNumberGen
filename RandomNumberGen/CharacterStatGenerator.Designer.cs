67866309bff176d1e57eb2846d1e273a189ac5d9
Alexis Dissell <akdissell@gmail.com>
2024-06-09 20:19:40 -0500

    Add project files.

---
+++ RandomNumberGen/CharacterStatGenerator.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace RandomNumberGen
+{
+    partial class CharacterStatGenerator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Stat1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Stat1
+            // 
+            this.Stat1.AutoSize = true;
+            this.Stat1.Location = new System.Drawing.Point(68, 31);
+            this.Stat1.Name = "Stat1";
+            this.Stat1.Size = new System.Drawing.Size(33, 13);
+            this.Stat1.TabIndex = 0;
+            this.Stat1.Text = "blank";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(71, 87);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // CharacterStatGenerator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Stat1);
+            this.Name = "CharacterStatGenerator";
+            this.Text = "Careter Sheet";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Stat1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ RandomNumberGen/CharacterStatGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace RandomNumberGen
+{
+    public partial class CharacterStatGenerator : Form
+    {
+        public CharacterStatGenerator()
+        {
+            InitializeComponent();
+        }
+        private int RollStats() 
+        {
+            var dice = new Random();
+            int stat;
+            stat = dice.Next(1, 7);
+            return stat;
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int ReturnedStat;
+            ReturnedStat =(int) RollStats();
+            Stat1.Text = ReturnedStat.ToString();
+        }
+    }
+}
